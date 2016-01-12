943e172de1eec1318cb47756626a8e55cf748ab0
Erik Tolentino and nic netzel <tolentino3575@gmail.com>
2016-01-12 11:55:25 -0800

    add project

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  $("#blanks form").submit(function(event) {
+    var yourNameInput = $("input#yourName").val();
+
+  $(".yourName").text(yourNameInput);
+  $("#letter").show();
+  event.preventDefault();
+
+  });
+});
